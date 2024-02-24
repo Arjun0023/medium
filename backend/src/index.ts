98cd80cf9faa131ce3b768ec1c8c7f0333eeefdd
Arjun Pawar <108388620+Arjun0023@users.noreply.github.com>
2024-02-24 19:38:30 +0530

    database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,26 @@ import { Hono } from 'hono';
 
 const app = new Hono();
 
+app.post('/api/v1/signup' , (c)=>{
+  return c.text('signup route')
+})
 
+app.post('/api/v1/signin', (c) => {
+	return c.text('signin route')
+})
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/api/v1/blog/:id', (c) => {
+  const id = c.req.param('id')
+  console.log(id);
+  return c.text('get blog route')
+})
+
+app.post('/api/v1/blog', (c)=>{
+  return c.text('signin route')
+})
+
+app.put('/api/v1/blog', (c) => {
+	return c.text('signin route')
 })
 
 export default app
