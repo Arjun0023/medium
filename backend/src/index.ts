8646f6d4e4f08ea598942251a74af6aab2b756e9
Arjun Pawar <108388620+Arjun0023@users.noreply.github.com>
2024-02-24 23:30:50 +0530

    asd

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,34 @@
 import { Hono } from 'hono';
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign } from 'hono/jwt';
+import { sign, verify } from 'hono/jwt';
 
 const app = new Hono<{
 	Bindings: {
 		DATABASE_URL: string
     JWT_SECRET: string,
-	}
+	},
+  Variables:{
+    userId:string
+  }
 }>();
+
+app.use('/api/v1/blog/*', async (c, next) => {
+	const jwt = c.req.header('Authorization');
+	if (!jwt) {
+		c.status(401);
+		return c.json({ error: "unauthorized" });
+	}
+	const token = jwt.split(' ')[1];
+	const payload = await verify(token, c.env.JWT_SECRET);
+	if (!payload) {
+		c.status(401);
+		return c.json({ error: "unauthorized" });
+	}
+	c.set('userId', payload.id);
+	await next()
+})
+
 
 app.post('/api/v1/signup', async (c) => {
 	const prisma = new PrismaClient({
@@ -58,8 +78,23 @@ app.get('/api/v1/blog/:id', (c) => {
   return c.text('get blog route')
 })
 
-app.post('/api/v1/blog', (c)=>{
-  return c.text('signin route')
+app.post('/api/v1/blog', async (c)=>{
+  const userId = c.get('userId');
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const post = await prisma.post.create({
+    data:{
+      title: body.title,
+      content: body.content,
+      authorId:userId
+    }
+  });
+  return c.json ({
+    id: post.id
+  });
 })
 
 app.put('/api/v1/blog', (c) => {
