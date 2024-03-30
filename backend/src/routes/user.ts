a18621a89a3736731adac3325045108eef258e90
Arjun Pawar <108388620+Arjun0023@users.noreply.github.com>
2024-03-30 23:40:06 +0530

    minor

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { sign } from 'hono/jwt'
-//import { signupInput, signinInput } from "";
+import { signupInput, signinInput } from "@100xdevs/medium-common";
 
 export const userRouter = new Hono<{
     Bindings: {
